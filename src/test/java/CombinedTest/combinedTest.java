1ce890f195a78275e27ee63bcf882f97daec4bdb
devaanshmann <devaanshmann5@gmail.com>
2025-10-17 12:17:28 -0700

    Combined Tests passed

---
+++ src/test/java/CombinedTest/combinedTest.java
@@ -0,0 +1,9 @@
+package CombinedTest;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"Bridge", "Decorator"})
+public class combinedTest {
+}
